fe3cfc12513c30a22942e531b414375fcfd58fcd
korisu07 <Hallo.Dreamy@gmail.com>
2021-07-23 17:35:50 +0900

    First commit. Create setting for scss compile.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// gulpプラグインを読み込み
+const { src, dest, watch } = require("gulp");
+
+// Sassをコンパイルするプラグインを読み込み
+const sass = require("gulp-sass");
+
+// ★Sassをコンパイルします
+
+const compileSass = function () {
+  //.scssファイルを取得
+  return (
+    src("./**/css/*.css")
+      // .cssファイルに圧縮済みの設定でコンパイル
+      .pipe(
+        sass({ outputStyle: "compressed" })
+          .on("error", sass.logError)
+      )
+      // cssファイルに保存
+      .pipe(dest("./"))
+  )
+}
+
+const watchSassFiles = function () {
+  watch("**/scss/*.scss", compileSass)
+}
+
+// npx gulpコマンドを実行した時、ファイルが監視されるようにします
+exports.default = watchSassFiles;
